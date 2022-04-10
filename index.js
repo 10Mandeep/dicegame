5a4509ec6bcf365f9c9c3f9c619a541503953542
Mandeep singh <98887284+10Mandeep@users.noreply.github.com>
2022-04-10 16:54:26 +0530

    update index.js
    update images to png

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 var randomNumber2 = Math.floor(Math.random()*6) + 1;
 
-var images1 = "images/dice"+ randomNumber1 +".png";
-var images2  = "images/dice"+ randomNumber2 +".png";
+var images1 = "dice"+ randomNumber1 +".png";
+var images2  = "dice"+ randomNumber2 +".png";
 
 
 document.querySelector(".dice .img1").setAttribute("src",images1);
