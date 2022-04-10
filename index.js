56f705b3a5b85df68855042997c4e89a5429a4e2
Mandeep singh <98887284+10Mandeep@users.noreply.github.com>
2022-04-10 16:51:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var images1 = "images/dice"+ randomNumber1 +".png";
+var images2  = "images/dice"+ randomNumber2 +".png";
+
+
+document.querySelector(".dice .img1").setAttribute("src",images1);
+document.querySelector(".dice .img2").setAttribute("src",images2);
+
+if ( randomNumber1 > randomNumber2 ) {
+document.getElementById("refresh").innerHTML = ("Player 1 😘 winn");
+}
+if ( randomNumber1 < randomNumber2 ) {
+    document.getElementById("refresh").innerHTML = ("Player 2 😘 winn");
+} else if ( randomNumber1 === randomNumber2 ) {
+    document.getElementById("refresh").innerHTML = ("DRAW.💩");
+}
